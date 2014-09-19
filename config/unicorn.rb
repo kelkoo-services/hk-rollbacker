366bf49093b5d2d27b70502f5ae34a63cf62ad02
ant30 <ant30tx@gmail.com>
2014-09-19 12:54:23 +0000

    Put sidekiq workers into web dyno context, so I don't want a worker dyno type in heroku

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,3 +1,9 @@
 worker_processes 1 || ENV['UNICORNS']
 timeout 30
 preload_app true
+
+sidekiq_workers = 1 || ENV['SIDEKIQ_WORKERS']
+
+before_fork do |server, worker|
+  @sidekiq_pid ||= spawn("bundle exec sidekiq -r ./app.rb -c #{sidekiq_workers}")
+end
