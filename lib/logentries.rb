56744c1c709f2a34467a99c687877783bd7d362d
ant30 <ant30tx@gmail.com>
2014-09-23 09:59:09 +0000

    Making the LE HMAC more close to raw

--- lib/logentries.rb
+++ lib/logentries.rb
@@ -14,11 +14,17 @@ def logentries_login(le_user, le_password, request)
   @auth ||= Rack::Auth::Basic::Request.new(request.env)
 
 
-  logger.info "No auth or credentials provided" unless (@auth.provided? && @auth.credentials)
-  return false unless (@auth.provided? && @auth.credentials)
- 
-  request_signature = @auth.credentials[1] 
-  request_user = @auth.credentials[0]
+  logger.info "No auth or credentials provided" if !headers.contains?('Authorization')
+  return false if !headers.contains?('Authorization')
+  credentials = headers['Authorization'].split(':')
+  request_user = credentials[0].split(' ').last
+  request_signature = credentials[1]
+
+ #  logger.info "No auth or credentials provided" unless (@auth.provided? && @auth.credentials)
+ #  return false unless (@auth.provided? && @auth.credentials)
+ # 
+ #  request_signature = @auth.credentials[1] 
+ #  request_user = @auth.credentials[0]
  
 
   logger.info "request_user != le_user" if request_user != le_user
