b0fd8e5145b07788d96b351d79e929aed6981e68
ant30 <ant30tx@gmail.com>
2014-09-24 09:25:28 +0000

    Headers from request environment instead of headers field

--- lib/logentries.rb
+++ lib/logentries.rb
@@ -10,7 +10,7 @@ def logentries_login(le_user, le_password, request)
   payload_md5 = Base64.encode64(Digest::MD5.digest(request.body.read)).strip
   request.body.rewind
 
-  @auth ||= Rack::Auth::Basic::Request.new(request.env)
+  headers = request.env
 
   logger.info "---header---"
   logger.info "#{headers}"
@@ -20,16 +20,10 @@ def logentries_login(le_user, le_password, request)
 
   logger.info "No auth or credentials provided" unless headers.include?('Authorization')
   return false unless headers.include?('Authorization')
+
   credentials = headers['Authorization'].split(':')
   request_user = credentials[0].split(' ').last
   request_signature = credentials[1]
-
- #  logger.info "No auth or credentials provided" unless (@auth.provided? && @auth.credentials)
- #  return false unless (@auth.provided? && @auth.credentials)
- # 
- #  request_signature = @auth.credentials[1] 
- #  request_user = @auth.credentials[0]
- 
 
   logger.info "request_user != le_user" if request_user != le_user
   return false unless request_user == le_user
