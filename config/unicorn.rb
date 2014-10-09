c46b96ff6bd7818b17082d372ac2283dac449aaf
ant30 <ant30tx@gmail.com>
2014-10-09 08:24:35 +0000

    Allow external and internal sidekiq in heroku

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,5 +5,5 @@ preload_app true
 sidekiq_workers = 1 || ENV['SIDEKIQ_WORKERS']
 
 before_fork do |server, worker|
-  @sidekiq_pid ||= spawn("bundle exec sidekiq -r ./app.rb -c #{sidekiq_workers}")
+  @sidekiq_pid ||= spawn("bundle exec sidekiq -r ./app.rb -c #{sidekiq_workers}") if ENV['SIDEKIQ_INTERNAL'] == 'true'
 end
